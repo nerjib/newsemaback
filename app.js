61348d7661e61593947d5ba2ce6568cc1365db7f
nerjib <kabirnajib0@gmail.com>
2021-03-24 23:22:50 +0100

    twilio

--- app.js
+++ app.js
@@ -79,40 +79,34 @@ app.use((req, res, next) => {
   app.post('/sms', function(req,res){
     const { sessionId, serviceCode, phoneNumber, text } = req.body;
 
-    let response = "";
+  let response = "";
+let fid=""
+  if (text === "") {
+    console.log(text);
+    // This is the first request. Note how we start the response with CON
+    response = `CON Facility type
+        1. Solar Motorized Borehole
+        2. Handpump Borehole`;
+  } else if (text === "1") {
+    // Business logic for first level response
+    response = `CON Facility ID`;
+    fid= text;
+
+  }else if(text===`${fid}*2`){
+    response = `CON  What is the problem1?`;
+
+  }  else{
+    response = `CON  Is the facility faulty ${fid}
+    1.yes
+    2. no`;
+  }
   
-    if (text === "") {
-      console.log(text);
-      // This is the first request. Note how we start the response with CON
-      response = `CON What would you like to check
-          1. My account
-          2. My phone number`;
-    } else if (text === "1") {
-      // Business logic for first level response
-      response = `CON Choose account information you want to view
-          1. Account number
-          2. Account balance`;
-    } else if (text === "2") {
-      // Business logic for first level response
-      // This is a terminal request. Note how we start the response with END
-      response = `END Your phone number is ${phoneNumber}`;
-    } else if (text === "1*1") {
-      // This is a second level response where the user selected 1 in the first instance
-      const accountNumber = "ACC100101";
-      // This is a terminal request. Note how we start the response with END
-      response = `END Your account number is ${accountNumber}`;
-    } else if (text === "1*2") {
-      // This is a second level response where the user selected 1 in the first instance
-      const balance = "KES 10,000";
-      // This is a terminal request. Note how we start the response with END
-      response = `END Your balance is ${balance}`;
-    }
-  
-    // Print the response onto the page so that our SDK can read it
-    res.set("Content-Type: text/plain");
-    res.send(response);
-    // DONE!!!
-  })
+
+  // Print the response onto the page so that our SDK can read it
+  res.set("Content-Type: text/plain");
+  res.send(response);
+  // DONE!!!
+})
 
      
 app.get('/', function(req,res){
