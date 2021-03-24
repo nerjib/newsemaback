bcf1b7c16e84173b5226ee10a91943c55d8f103a
nerjib <kabirnajib0@gmail.com>
2021-03-24 15:27:43 +0100

    twilio

--- app.js
+++ app.js
@@ -77,22 +77,41 @@ app.use((req, res, next) => {
   });
   
   app.post('/sms', function(req,res){
-    console.log(req.body)
-    //console.log(req.body.body+' '+req.body.From)
-  //  console.log(req.From)
+    const { sessionId, serviceCode, phoneNumber, text } = req.body;
 
-/*
-  const twiml = new MessagingResponse();
+  let response = "";
 
-  twiml.message('The Robots are coming! Head for the hills!');
+  if (text === "") {
+    console.log(text);
+    // This is the first request. Note how we start the response with CON
+    response = `CON What would you like to check
+        1. My account
+        2. My phone number`;
+  } else if (text === "1") {
+    // Business logic for first level response
+    response = `CON Choose account information you want to view
+        1. Account number
+        2. Account balance`;
+  } else if (text === "2") {
+    // Business logic for first level response
+    // This is a terminal request. Note how we start the response with END
+    response = `END Your phone number is ${phoneNumber}`;
+  } else if (text === "1*1") {
+    // This is a second level response where the user selected 1 in the first instance
+    const accountNumber = "ACC100101";
+    // This is a terminal request. Note how we start the response with END
+    response = `END Your account number is ${accountNumber}`;
+  } else if (text === "1*2") {
+    // This is a second level response where the user selected 1 in the first instance
+    const balance = "KES 10,000";
+    // This is a terminal request. Note how we start the response with END
+    response = `END Your balance is ${balance}`;
+  }
 
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());*/
- // console.log(twiml)
- res.set("Content-Type: text/plain");
-  res.send('response');
- ////return ('hello')
- //res.write('fff')
+  // Print the response onto the page so that our SDK can read it
+  res.set("Content-Type: text/plain");
+  res.send(response);
+  // DONE!!!
 })
 
      
