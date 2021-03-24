6a69e9a5b45bf9c6afe5ae5b2531576ec60df28e
nerjib <kabirnajib0@gmail.com>
2021-03-24 10:56:30 +0100

    twilio

--- src/db/index.js
+++ src/db/index.js
@@ -8,9 +8,7 @@ dotenv.config();
 
 const client = new Client({
   connectionString: process.env.DATATYPE === 'test' ? process.env.DATABASE_URL1 : process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+
 });
 
 client.connect();
