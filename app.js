efa8cd28deac3ba0149e6f8037c20bf06570f52b
nerjib <kabirnajib0@gmail.com>
2021-03-24 21:51:21 +0100

    twilio

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.use((req, res, next) => {
         2. Handpump Borehole`;
   } else if (text === "1") {
     // Business logic for first level response
-    response = `CON Facility ID`;
+    response = `END Facility ID`;
   }  else{
     response = `CON  Is the facility faulty
     1.yes
