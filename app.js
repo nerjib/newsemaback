3fe368b160a141ec93763efe749d8c4864b7856c
nerjib <kabirnajib0@gmail.com>
2021-03-29 09:54:18 +0100

    twilio

--- app.js
+++ app.js
@@ -255,88 +255,8 @@ let response = "";
 if(text === ''){
 message = welcomeMsg
 
-}else if(textValue === 1){
-if(text.split('*')[0]==='2'){
-  lang = 2
-  message = `CON Wani irin famfo ne?
-  1. Famfon tuka-tuka
-  2. Famfon sola`
-}else {
-  lang = 1
-  message = `CON Select facility type
-  1. Handpump borehole
-  2. Solar motorized Borehole`
-}
-
-}else if(textValue === 2){
-  if (lang === 2){
-    message = `CON Saka lambar famfo` 
-    if(text.split('*')[1] === "1"){
-      facilityDetails.facility='Famfon tuka-tuka'
-    facility='Famfon tuka-tuka'
-  
-    }else if (text.split('*')[1] === "2"){
-      facilityDetails.facility='Famfon sola'
-      facility='Famfon sola'    
-    }     
-  }else{
-    message = "CON Input facility code"
-     if(text.split('*')[1] === "1"){
-      facilityDetails.facility='Handpump Borehole'
-    facility='Handpump Borehole'
-  
-    }else if (text.split('*')[1] === "2"){
-      facilityDetails.facility='Solar Motorize Borehole'
-      facility='Solar Motorize Borehole'
-  
-    }
-
-  }
-
-
-}else if(textValue === 3){
-facilityDetails.fid= text.split('*')[2]
-
-if (lang === 2){
-  message = `CON Menene matsalan famfo
-  1. Baya aiki gaba daya
-  2. Famfo baya bada wadataccen ruwa
-  3. Ruwa baya taruwa a tanki
-  4. Tuka-tuka na wahalar bugawa
-  5. Bututun ruwa ya fashe
-  6. Allon sola ya fita
-  7. wani matsala daban` 
- 
 }else{
-  message = `CON What is the problem?
-  1. Not working completely
-  2. Low yield
-  3. Water not pumpimg to tank
-  4. Hard to pump
-  5. Leaking pipe
-  6. Solar panel vandalized
-  7. others`
-
-
-}
-
-}
-else{
-if (lang===2){
-  message = `END Mun gode da wannan bayanin akan ${facilityDetails.facility} 
-  Lambar famfo: ${facilityDetails.fid}
-  Matsalar famfo:   ${text.split('*')[3]==='1'?"Baya aiki gaba daya":text.split('*')[3]==='2'?"Ba wadataccen":text.split('*')[3]==='3'?"ruwa baya taruwa":text.split('*')[3]==='4'?"Tuka-tuka na wahalar bugawa":text.split('*')[3]==='5'?"Bututun ruwa ya fashe":text.split('*')[3]==='6'?"Allon sola ya fita":"Wani matsalan daban"}`
-}else{
-message = `END Thanks for your feedback on ${facilityDetails.facility} with details
- facility Id: ${facilityDetails.fid}
- fault:   ${text.split('*')[3]==='1'?"Not working completely":text.split('*')[3]==='2'?"Low water yield":text.split('*')[3]==='3'?"water not pumping to tank":text.split('*')[3]==='4'?"Hard to pump":text.split('*')[3]==='5'?"Leaking pipe":text.split('*')[3]==='6'?"Solar panel vandalized":"Wani matsalan daban"}`
-}
- fault = text.split('*')[3]==='1'?"Baya aiki gaba daya":text.split('*')[3]==='2'?"Ba wadataccen":text.split('*')[3]==='3'?"ruwa baya taruwa":"Wani matsalan daban";
-facilityDetails.fault= text.split('*')[3]==='1'?"Baya aiki gaba daya":text.split('*')[3]==='2'?"Ba wadataccen":text.split('*')[3]==='3'?"ruwa baya taruwa":"Wani matsalan daban"
-  console.log(fid)
-  console.log(facility)
-  console.log(facilityDetails)
-
+  message = 'END bye'
 }
 
 // Print the response onto the page so that our SDK can read it
